984a646ebdaa3d4c8392df4d2917a01a58154014
BerillVarga <118910164+BerillVarga@users.noreply.github.com>
2023-04-12 16:13:29 +0200

    Update streamlit_app.py
    added default values to the pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Creating a pick list 
-streamlit.multiselect('Pick same fruits: ', list(my_fruit_list.index))
+streamlit.multiselect('Pick same fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # Display dataframe in the app
 streamlit.dataframe(my_fruit_list)
