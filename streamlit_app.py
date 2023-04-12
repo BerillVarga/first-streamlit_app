85e40838e80e92a3358c5eec3dbc9183e2b2ab78
BerillVarga <118910164+BerillVarga@users.noreply.github.com>
2023-04-12 16:00:24 +0200

    Update streamlit_app.py
    loaded data from S3 buckets and displayed it in the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import modules
 import streamlit
+import pandas
 
 # Streamlit title
 streamlit.title('Mel\'s Parents\' Healthy Diner')
@@ -13,3 +14,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 # Creating smoothie building section
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Loadning data
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+# Display dataframe in the app
+streamlit.dataframe(my_fruit_list)
