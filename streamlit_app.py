3787f4675c62500527c84384d38e65202f99366d
BerillVarga <118910164+BerillVarga@users.noreply.github.com>
2023-04-12 16:07:13 +0200

    Update streamlit_app.py
    added pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Loadning data
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
+# Creating a pick list 
+streamlit.multiselect('Pick same fruits: ', list(my_fruit_list.index))
+
 # Display dataframe in the app
 streamlit.dataframe(my_fruit_list)
