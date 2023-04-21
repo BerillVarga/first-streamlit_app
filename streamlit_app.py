7985363f811efd1d1ca1c667a5756acd02b6e1a8
BerillVarga <118910164+BerillVarga@users.noreply.github.com>
2023-04-21 15:46:22 +0200

    Fixed error in requests call

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,5 +32,5 @@ fruits_to_show = my_fruit_list.loc[fuits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section with fruit data from Fruityvice API response
-fruityvice_response = request.get('https://fruityvice.com/api/fruit/watermelon')
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 streamlit.text(fruityvice_response)
