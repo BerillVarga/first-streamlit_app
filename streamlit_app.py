2519027da8552cdda7c94854690c580a808232db
BerillVarga <118910164+BerillVarga@users.noreply.github.com>
2023-04-22 16:49:42 +0200

    changed meta data selection to data selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,10 +56,10 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-# Selecting some Snowflake meta data
+# Selecting date from Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list constains:")
 streamlit.text(my_data_row)
